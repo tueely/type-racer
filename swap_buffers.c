bf47aff193dbeb7a098f6f803036ece902f1291c
Ayush Ranjan <ayush.ranjan@mail.utoronto.ca>
2025-03-28 13:06:05 -0400

    Update and rename swap_buffers to swap_buffers.c

--- swap_buffers.c
+++ swap_buffers.c
@@ -3,3 +3,8 @@ void swap_buffers() {
   *(volatile uint32_t*)(FB_CONTROLLER + 4) = (uint32_t)back_buffer;
   *(volatile uint32_t*)FB_CONTROLLER = 1;
 }
+
+void clear_backbuffer() {
+  for (int y = 0; y < FB_HEIGHT; y++)
+    for (int x = 0; x < FB_WIDTH; x++) back_buffer[y][x] = COLOR_BLACK;
+}
